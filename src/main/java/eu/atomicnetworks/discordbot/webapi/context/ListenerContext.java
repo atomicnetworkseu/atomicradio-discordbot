b415ed11c3089fc886d77fbf0adaf96b8bcc8a4c
Kacper Mura <kacper.mura@web.de>
2021-04-12 19:38:36 +0200

    Update ListenerContext.java

--- src/main/java/eu/atomicnetworks/discordbot/webapi/context/ListenerContext.java
+++ src/main/java/eu/atomicnetworks/discordbot/webapi/context/ListenerContext.java
@@ -1,7 +1,9 @@
 package eu.atomicnetworks.discordbot.webapi.context;
 
 import com.sun.net.httpserver.HttpServer;
+import eu.atomicnetworks.discordbot.object.Listeners.Listener;
 import eu.atomicnetworks.discordbot.webapi.ApiServer;
+import java.util.Map;
 import org.json.JSONObject;
 
 /**
@@ -35,7 +37,30 @@ public class ListenerContext {
             
             JSONObject json = new JSONObject(this.apiServer.getDiscordBot().getGson().toJson(this.apiServer.getDiscordBot().getBackendManager().getListeners()));
             int listener = this.apiServer.getDiscordBot().getBackendManager().getListeners().getListener().size();
+            int oneListener = 0;
+            int danceListener = 0;
+            int trapListener = 0;
+            
+            for(Map.Entry<String, Listener> listenerEntry : this.apiServer.getDiscordBot().getBackendManager().getListeners().getListener().entrySet()) {
+                switch(listenerEntry.getValue().getStation()) {
+                    case "one":
+                        oneListener = oneListener+1;
+                        break;
+                    case "dance":
+                        danceListener = danceListener+1;
+                        break;
+                    case "trap":
+                        trapListener = trapListener+1;
+                        break;
+                    default:
+                        oneListener = oneListener+1;
+                        break;
+                }
+            }
             json.put("listeners", listener);
+            json.put("one_listeners", oneListener);
+            json.put("dance_listeners", danceListener);
+            json.put("trap_listeners", trapListener);
             this.apiServer.sendResponseJson(he, 200, json);
         });
     }
